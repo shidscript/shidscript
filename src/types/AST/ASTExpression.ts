acd5e82d9a125052a33794e0a6f33eb741fd35ca
Palm <palmpasuthorn@gmail.com>
2022-03-24 21:35:19 +0700

    fix(ASTExpression): Comment undone file imports

--- src/types/AST/ASTExpression.ts
+++ src/types/AST/ASTExpression.ts
@@ -1,6 +1,6 @@
-import { StringLiteral, NumberLiteral, FixedPointNumberLiteral, BigNumberLiteral, BooleanLiteral, ObjectLiteral, ArrayLiteral, FunctionLiteral, ObjectPattern, ArrayPattern } from '../../classes/AST'
+import { StringLiteral, NumberLiteral, FixedPointNumberLiteral, BigNumberLiteral, BooleanLiteral, ObjectLiteral, ArrayLiteral, /* FunctionLiteral,*/ ObjectPattern, ArrayPattern } from '../../classes/AST'
 
-export type Any = StringLiteral | NumberLiteral | FixedPointNumberLiteral | BigNumberLiteral | BooleanLiteral | ObjectLiteral | ArrayLiteral | FunctionLiteral
+export type Any = StringLiteral | NumberLiteral | FixedPointNumberLiteral | BigNumberLiteral | BooleanLiteral | ObjectLiteral | ArrayLiteral /*| FunctionLiteral*/
 export type Numbers = NumberLiteral | FixedPointNumberLiteral | BooleanLiteral
-export type Objects = ObjectLiteral | ArrayLiteral | FunctionLiteral
+export type Objects = ObjectLiteral | ArrayLiteral /*| FunctionLiteral*/
 export type Patterns = ObjectPattern | ArrayPattern
